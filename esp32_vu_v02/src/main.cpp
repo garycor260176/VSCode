92d9c10b4afafad7e6c81ea46f61a3f6ad288edc
garycor260176 <ipolnikov@gmail.com>
2025-03-20 00:37:40 +0300

    202503219

---
+++ esp32_vu_v02/src/main.cpp
@@ -0,0 +1,486 @@
+/********************************************************************************************************************************************************
+*                                                                                                                                                       *
+   Project:         Analog 10 Channel Spectrum Analyzer
+   Target Platform: ESP32
+*                                                                                                                                                       *
+   Version: 1.0
+   Hardware setup: See github
+   Spectrum analyses done with analog discrete components. AKA Filters
+*                                                                                                                                                       *
+   Mark Donners
+   The Electronic Engineer
+   Website:   www.theelectronicengineer.nl
+   facebook:  https://www.facebook.com/TheelectronicEngineer
+   youtube:   https://www.youtube.com/channel/UCm5wy-2RoXGjG2F9wpDFF3w
+   github:    https://github.com/donnersm
+*                                                                                                                                                       *
+*********************************************************************************************************************************************************
+  Version History
+  1.0   Webinterface included on first core while rest runs on second core. This way WIFI low signal doesn's freeze up the program
+        VU meter sensitivity is linked to Sense potmeter like with the overal sensitivity
+        Webserver setup can be forced on startup by holding down the mode button
+*/
+#define numBands 10  // number of bands
+#define VERSION     "V1.0"
+
+//libraries
+#include <FastLED_NeoMatrix.h>
+#include <Adafruit_GFX.h>
+#include <pixeltypes.h>
+#include <espasyncbutton.hpp>
+
+// included dependency files
+#include "LEDDRIVER.H"
+#include "Settings.h"
+#include "PatternsLedstrip.h"
+#include "fire.h"
+
+// no need to change any of the parameters below, all you can change is in settings.h
+#define   up                  1                         // just to make Ardduino life easier
+#define   down                0                         // just to make Ardduino life easier
+int       PeakDirection =     0;                        // peaks can be displayed falling or floating away. 
+long      LastDoNothingTime = 0;                        // only needed for screensaver
+int       DemoModeMem =       0;                        // to remember what mode we are in when going to demo, in order to restore it after wake up
+bool      AutoModeMem =       false;                    // same story
+bool      DemoFlag =          false;                    // we need to know if demo mode was manually selected or auto engadged.
+bool      webtoken =          false;                    // this is a flag so that the webserver noise when the other core has new data
+int       noise =             0;                        // default, overruled by setuploop
+int       VUadc =             0;                        // we'll need it don't change
+int       SENSE =             60;                       // Sensitivity. overruled by potmeter 
+int       gVU       =         0;                        // Instantaneous read of VU value
+int       oldVU     =         0;                        // Previous read of VU value
+int FreqBins[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};   // these will contain the amplitude values for the frequency columns
+int FreqBinsOld[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};// these will contain the amplitude values for the frequency columns
+int FreqBinsNew[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};// these will contain the amplitude values for the frequency columns
+int oldBarHeights[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // so they are set to 11
+char PeakFlag[numBands];                                // the top peak delay needs to have a flag because it has different timing while floating compared to falling to the stack
+int PeakTimer[numBands];                                // counter how many loops to stay floating before falling to stack
+
+TaskHandle_t LogoTask;
+
+//************************************************************************
+//************ Button related stuff **************************************
+//** see also 'Attach callback to buttons' in setup function          //**
+// Two buttons                                                        //**
+AsyncEventButton ModeBut(MODE_BUTTON_PIN, HIGH);
+
+void SetInput(int inputpin);
+void Analyser_ALL();
+void DrawVUMeter(int yVU);
+void Matrix_Flag();
+
+void setup_ModeBut(){
+  ModeBut.begin();
+  // make debounce a bit more aggresive
+  ModeBut.timeouts.setDebounce(20000);
+
+  // on press print help message
+  ModeBut.onClick([](){
+    buttonPushCounter = (buttonPushCounter + 1) % NumberOfModes; //%6   //**
+    FastLED.clear();                                                    //**
+  });
+
+  ModeBut.onLongPress([](){
+  });
+
+  ModeBut.onMultiClick([](int32_t cnt){
+      switch(cnt){
+          case 2 :
+          break;
+          case 3 :
+          break;
+          case 4 :
+          break;
+      }
+  });
+
+  ModeBut.enable();  
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(500);
+  Serial.print("Main program is  running on core ");
+  Serial.println(xPortGetCoreID());
+  Serial.print("Version#:");
+  Serial.println(VERSION);
+  // Initialize the button.
+  setup_ModeBut();
+  
+  pinMode(S0, OUTPUT);  // Pin for S0 Multitplexor
+  pinMode(S1, OUTPUT);  // Pin for S1 Multitplexor
+  pinMode(S2, OUTPUT);  // Pin for S2 Multitplexor
+  pinMode(MP_E, OUTPUT);  // Pin for Enable Multiplexor When High muliplexor=disabled
+  pinMode(S3, OUTPUT);  // Pin for S3 Multitplexor
+
+  // setup the ledstrip
+  SetupLEDSTRIP();
+  // setup the correct input line or mic
+  SetInput(InputLine); // select input source on startup
+}
+
+
+void loop() {
+  // wm.process();
+  size_t bytesRead = 0;
+  int TempADC = 0;
+
+  //############  Handle user interface ##################
+
+  // read potmeters and process
+  PEAKDELAY = map(analogRead(PEAKDELAYPOT), 0, 4095, 100, Fallingspeed);
+  SENSE = map(analogRead(SENSEPOT), 0, 4095, 1, InputBoost);
+  BRIGHTNESS = map(analogRead(BRIGHTNESSPOT), 0, 4095, BRIGHTNESSMAX, BRIGHTNESSMIN);
+  FastLED.setBrightness(BRIGHTNESS);
+
+  // now get latest band data 
+  Analyser_ALL(); // call function to read all channels to fill FreqBinsNew[]
+  
+  // Process the read data
+  // compare to previous values. If gone up then oldvalue is overwritten with new value
+  // if gone down then newvalue is oldvalue-speedfilter ( this will reduce noise glitches and smooths out the display)
+  float averageSum = 0;
+  for (int cnt = 0; cnt < numBands; cnt++) {
+    if (FreqBinsNew[cnt] < FreqBinsOld[cnt]) {
+      FreqBins[cnt] = max(FreqBinsOld[cnt] - Speedfilter, FreqBinsNew[cnt]);
+    }
+    else if (FreqBinsNew[cnt] > FreqBinsOld[cnt]) {
+      FreqBins[cnt] = FreqBinsNew[cnt];
+    }
+    FreqBinsOld[cnt] = FreqBins[cnt];
+    // using this same loop to do some processing
+    // like scaling the freqbins to matrix height
+    // if VUMeter is displayed then matrix height -1
+    //   Serial.printf("%d,",FreqBins[cnt]);
+    if (VUMeter == 1)   FreqBins[cnt] = map(FreqBins[cnt], 0, SENSE, 0, kMatrixHeight - 1);
+    else FreqBins[cnt] = map(FreqBins[cnt], 0, SENSE, 0, kMatrixHeight);
+    //   if(FreqBins[cnt]>gVU)gVU=FreqBins[cnt];
+
+  }
+
+  // Value for VU meter processing
+  gVU = (gVU * 400) / SENSE; // this makes the VU scale depend on the Sense potmeter
+  if (VUadc < oldVU)gVU--;
+  else gVU = VUadc;
+  oldVU = VUadc;
+  if (abs(gVU) > DemoTreshold)LastDoNothingTime = millis(); // if there is signal in any off the bands[>treshhold] then no demo mode
+
+
+  // Process the BINdata into bar heights
+  for (int band = 0; band < numBands; band++) {
+    int barHeight = FreqBins[band];
+    if (VUMeter == 1) {
+      if (barHeight > TOP - 1) barHeight = TOP - 1; // don't want to mess up the VU meter
+    }
+    // Small amount of averaging between frames
+    barHeight = ((oldBarHeights[band] * 1) + barHeight) / 2;
+
+    // Move peak up
+    if (barHeight > peak[band]) {
+      peak[band] = min(TOP, barHeight);
+      PeakFlag[band] = 1;
+    }
+
+
+    // if there hasn't been much of a input signal for a longer time ( see settings ) go to demo mode
+    if ((millis() - LastDoNothingTime) > DemoAfterSec && DemoFlag == false)
+    { 
+      DemoFlag = true;
+      // first store current mode so we can go back to it after wake up
+      DemoModeMem = buttonPushCounter;
+      AutoModeMem = autoChangePatterns;
+      autoChangePatterns = false;
+      buttonPushCounter = 12;
+      Serial.println("Automode is turned of because of demo");
+    }
+    // Wait,signal is back? then wakeup!
+    else if (DemoFlag == true &&   (millis() - LastDoNothingTime) < DemoAfterSec   )
+    { 
+      // while in demo the democounter was reset due to signal on one of the bars.
+      // So we need to exit demo mode.
+      buttonPushCounter = DemoModeMem; // restore settings
+      Serial.printf ("automode setting restored to: %d", AutoModeMem);
+      autoChangePatterns = AutoModeMem; // restore settings
+      DemoFlag = false;
+      FastLED.clear();
+    }
+
+    #if BottomRowAlwaysOn
+       if (barHeight == 0)barHeight = 1; // make sure there is always one bar that lights up
+    #endif
+    // Now visualize those bar heights
+    switch (buttonPushCounter) {
+      case 0:
+        changingBarsLS(band, barHeight);
+        break;
+      case 1:
+        TriBarLS(band, barHeight);
+        TriPeakLS(band);
+        break;
+      case 2:
+        rainbowBarsLS(band, barHeight);
+        NormalPeakLS(band, PeakColor1);
+        break;
+      case 3:
+        purpleBarsLS(band, barHeight);
+        NormalPeakLS(band, PeakColor2);
+        break;
+      case 4:
+        SameBarLS(band, barHeight);
+        NormalPeakLS(band, PeakColor3);
+        break;
+      case 5:
+        SameBar2LS(band, barHeight);
+        NormalPeakLS(band, PeakColor3);
+        break;
+      case 6:
+        centerBarsLS(band, barHeight);
+        break;
+      case 7:
+        centerBars2LS(band, barHeight);
+        break;
+      case 8:
+        centerBars3LS(band, barHeight);
+        break;
+      case 9:
+        BlackBarLS(band, barHeight);
+        outrunPeakLS(band);
+        break;
+      case 10:
+        BlackBarLS(band, barHeight);
+        NormalPeakLS(band, PeakColor5);
+        break;
+      case 11:
+        BlackBarLS(band, barHeight);
+        TriPeak2LS(band);
+        break;
+      case 12:
+        matrix->fillRect(0, 0, matrix->width(), 1, 0x0000); // delete the VU meter
+        make_fire();
+        break;
+    }
+    // Save oldBarHeights for averaging later
+    oldBarHeights[band] = barHeight;
+  }
+
+  if (VUMeter == true) {
+    if (buttonPushCounter != 12) DrawVUMeter(0); // Draw it when not in screensaver mode
+  }
+
+
+  // Decay peak
+  EVERY_N_MILLISECONDS(Fallingspeed) {
+
+    for (byte band = 0; band < numBands; band++) {
+      if (PeakFlag[band] == 1) {
+        PeakTimer[band]++;
+        if (PeakTimer[band] > PEAKDELAY) {
+          PeakTimer[band] = 0;
+          PeakFlag[band] = 0;
+        }
+      }
+      else if ((peak[band] > 0) && (PeakDirection == up)) {
+        peak[band] += 1;
+        if (peak[band] > (kMatrixHeight + 10))peak[band] = 0;
+      } // when to far off screen then reset peak height
+      else if ((peak[band] > 0) && (PeakDirection == down)) {
+        peak[band] -= 1;
+      }
+    }
+    colorTimer++;
+  }
+
+  EVERY_N_MILLISECONDS(10)colorTimer++; // Used in some of the patterns
+
+  EVERY_N_SECONDS(SecToChangePattern) {
+    if (autoChangePatterns) {
+      buttonPushCounter = (buttonPushCounter + 1) % 12;
+    }
+  }
+
+  delay(1);                         // needed to give fastled a minimum recovery time
+  FastLED.show();
+  webtoken = true;                  // set marker so that other core can process data
+
+} // loop end
+
+void DrawVUPixels(int i, int yVU, int fadeBy = 0) {
+  CRGB VUC;
+  if (i > (PANE_WIDTH - 3)) {
+    VUC.r = 255;
+    VUC.g = 0;
+    VUC.b = 0 ;
+  }
+  else if (i > (PANE_WIDTH / 5)) {
+    VUC.r = 255;
+    VUC.g = 255;
+    VUC.b = 0;
+  }
+  else { // green
+    VUC.r = 0;
+    VUC.g = 255;
+    VUC.b = 0;
+  }
+  if (DoubleVU == true) {
+    // dbgprint("i=%d", i);
+    if (i > ((PANE_WIDTH / 2) - 2)) { // little overrule
+      VUC.r = 255;
+      VUC.g = 0;
+      VUC.b = 0 ;
+    }
+    int xHalf = matrix->width() / 2;
+    matrix->drawPixel(xHalf - i - 1, yVU, CRGB(VUC.r, VUC.g, VUC.b).fadeToBlackBy(fadeBy));
+    matrix->drawPixel(xHalf + i,   yVU, CRGB(VUC.r, VUC.g, VUC.b).fadeToBlackBy(fadeBy));
+  }
+  else {
+    matrix->drawPixel( i,   yVU, CRGB(VUC.r, VUC.g, VUC.b).fadeToBlackBy(fadeBy));
+  }
+}
+
+
+void DrawVUMeter(int yVU) {
+  static int iPeakVUy = 0;                                  // size (in LED pixels) of the VU peak
+  static unsigned long msPeakVU = 0;                        // timestamp in ms when that peak happened so we know how old it is
+  const int MAX_FADE = 256;
+  matrix->fillRect(0, yVU, matrix->width(), 1, 0x0000);
+  if (iPeakVUy > 1) {
+    int fade = MAX_FADE * (millis() - msPeakVU) / (float) 1000;
+    DrawVUPixels(iPeakVUy,   yVU, fade);
+  }
+  int xHalf ;
+  if (DoubleVU == true)xHalf = (PANE_WIDTH / 2); // - 1;
+  else xHalf = PANE_WIDTH;
+  int bars  = map(gVU, 0, MAX_VU, 1, xHalf);
+  bars = min(bars, xHalf);
+  //  bars = min(bars, xHalf*2);
+  if (bars > iPeakVUy) {
+    msPeakVU = millis();
+    iPeakVUy = bars;
+  }
+  else if (millis() - msPeakVU > 1000)iPeakVUy = 0;
+  for (int i = 0; i < bars; i++)DrawVUPixels(i, yVU);
+}
+
+
+//**************************************************************************************************
+//                                          D B G P R I N T                                        *
+//**************************************************************************************************
+// Send a line of info to serial output.  Works like vsprintf(), but checks the DEBUG flag.        *
+// Print only if DEBUG flag is true.  Always returns the formatted string.                         *
+// Usage dbgprint("this is the text you want: %d", variable);
+//**************************************************************************************************
+void dbgprint(const char * format, ...) {
+  if (DEBUG) {
+    static char sbuf[DEBUG_BUFFER_SIZE]; // For debug lines
+    va_list varArgs; // For variable number of params
+    va_start(varArgs, format); // Prepare parameters
+    vsnprintf(sbuf, sizeof(sbuf), format, varArgs); // Format the message
+    va_end(varArgs); // End of using parameters
+    if (DEBUG) // DEBUG on?
+    {
+      Serial.print("Debug: "); // Yes, print prefix
+      Serial.println(sbuf); // and the info
+    }
+    // return sbuf; // Return stored string
+  }
+}
+
+
+
+// inputSelect  use: SetInput(MIC) or SetInput(LINE)
+void SetInput(int inputpin) {
+  noise= noiseline;
+}
+
+// Muliplexor channel select
+void SetMultiPlexTo(int channelnumber) {
+  digitalWrite(MP_E, 1); // multiplex disabled
+  digitalWrite(S0, channelnumber & 0b0001);
+  digitalWrite(S1, channelnumber & 0b0010);
+  digitalWrite(S2, channelnumber & 0b0100);
+  digitalWrite(S3, channelnumber & 0b1000);
+  digitalWrite(MP_E, 0); // Multiplex enabled
+}
+
+void Analyser_ALL() {
+  int tempADC = 0;
+  //gVUalt=0;
+  for (int channelcnt = 0; channelcnt < numBands; channelcnt++) {
+    digitalWrite(MP_E, 1); // multiplex disabled
+    digitalWrite(S0, channelcnt & 0b0001);
+    digitalWrite(S1, channelcnt & 0b0010);
+    digitalWrite(S2, channelcnt & 0b0100);
+    digitalWrite(S3, channelcnt & 0b1000);
+    digitalWrite(MP_E, 0); // Multiplex enabled
+    tempADC = analogRead(ADC_IN);
+    FreqBinsNew[channelcnt] = max(tempADC - ADC_OFFSET - noise, 1); // remove offset
+    //gVUalt+= FreqBinsNew[channelcnt];
+  }
+  // gVUalt=gVUalt/10;
+  // now read the audio level for VU
+  digitalWrite(MP_E, 1); // multiplex disabled
+  digitalWrite(S0, 10 & 0b0001);
+  digitalWrite(S1, 10 & 0b0010);
+  digitalWrite(S2, 10 & 0b0100);
+  digitalWrite(S3, 10 & 0b1000);
+  digitalWrite(MP_E, 0); // Multiplex enabled
+  tempADC = analogRead(ADC_IN);
+  //  VUadc=max(tempADC-ADC_OFFSET,1);
+
+  VUadc = abs(VU_OFFSET - tempADC);
+  VUadc = max(VUadc - noise, 0);
+
+  //dbgprint("%d",gVUalt);
+}
+
+
+void startAutoMode() {
+  autoChangePatterns = true;
+  Matrix_Flag();                  //this is to show user that automode was engaged. It will show dutch flag for 2 seconds
+  delay(5000);
+  Serial.println(" Patterns will change after few seconds ");
+  Serial.println(" You can reset by pressing the mode button again");
+}
+
+void Matrix_Flag() {
+
+  int ledcounter = 0;
+  for (int i = 0; i < kMatrixWidth / 2; i++) {
+    for (int j = 0; j < kMatrixHeight / 3; j++) {
+      leds[ledcounter] = CRGB::Blue;
+      ledcounter++;
+    }
+    for (int j = 0; j < kMatrixHeight / 3; j++) {
+      leds[ledcounter] = CRGB::White;
+      ledcounter++;
+    }
+    for (int j = 0; j < kMatrixHeight / 3; j++) {
+      leds[ledcounter] = CRGB::Red;
+      ledcounter++;
+    }
+    // if number of rows can not be devided by 3 then compensate
+    for (int i = 0; i < (kMatrixHeight - (kMatrixHeight / 3) * 3); i++) {
+      leds[ledcounter] = CRGB::Red;
+      ledcounter++;
+    }
+    // if number of rows can not be devided by 3 then compensate
+    for (int i = 0; i < (kMatrixHeight - (kMatrixHeight / 3) * 3); i++) {
+      leds[ledcounter] = CRGB::Red;
+      ledcounter++;
+    }
+
+    for (int j = 0; j < kMatrixHeight / 3; j++) {
+      leds[ledcounter] = CRGB::Red;
+      ledcounter++;
+    }
+    for (int j = 0; j < kMatrixHeight / 3; j++) {
+      leds[ledcounter] = CRGB::White;
+      ledcounter++;
+    }
+    for (int j = 0; j < kMatrixHeight / 3; j++) {
+      leds[ledcounter] = CRGB::Blue;
+      ledcounter++;
+    }
+  }
+  FastLED.show();
+}
---
+++ esp32mini_levolo/src/main.cpp
@@ -0,0 +1,286 @@
+#include <mqtt_ini.h>
+#define FASTLED_ESP8266_RAW_PIN_ORDER
+#include <FastLED.h>
+#include <Preferences.h>
+
+#define def_path "switch/s_01"
+
+#define LED_PIN        16
+#define MAXNUMBUTTONS  4
+
+#define NUMLEDS        4
+CRGB leds[NUMLEDS];
+#define CURRENT_LIMIT 1000    // лимит по току в миллиамперах, автоматически управляет яркостью (пожалей свой блок питания!) 0 - выключить лимит
+
+Preferences preferences;
+
+mqtt_ini *client;
+
+int pins[MAXNUMBUTTONS] = {
+5,
+23,
+19,
+18,
+};
+ 
+struct stru_state{
+  int brightness;
+  int AutoSeconds;
+};
+
+stru_state curState;
+
+void report(stru_state, int);
+void onMsgCommand(const String &message);
+void OnCheckState();
+void onConnection();
+void OnLoad();
+stru_state read_eeprom();
+stru_state write_eeprom(stru_state newState);
+void createButtons();
+void setBrightness(int brgth);
+
+//***
+class Button {
+  public:
+    Button(int _pin, int _num);
+    String getName();
+    void setMode(int _mode);
+    void loop();
+
+  private:
+    int pin;
+    int mode;
+    int num;
+    String name;
+
+    uint32_t TimeSinceLastBtn = 0;
+    boolean flagSinceLastBtn = false;
+    
+    int read_mode();
+    int write_mode(int);
+    void setColor(uint8_t g, uint8_t r, uint8_t b);
+};
+
+void Button::loop(){
+  String _name = name + "/pressed";
+
+  int state = digitalRead(pin);
+  if(state == HIGH){
+    if(!flagSinceLastBtn){
+      flagSinceLastBtn = true;
+      TimeSinceLastBtn = millis( );
+    }
+  } else {
+    if(flagSinceLastBtn) {
+      uint32_t secs = curState.AutoSeconds * 1000;
+
+      if(millis( ) - TimeSinceLastBtn >= secs) {
+        client->Publish(_name.c_str(), String(2));
+      } else {
+        client->Publish(_name.c_str(), String(1));
+      } 
+      TimeSinceLastBtn = 0;
+    }
+    flagSinceLastBtn = false;
+  }
+}
+
+void Button::setColor(uint8_t g, uint8_t r, uint8_t b){
+  leds[num].setRGB( r, g, b);
+  FastLED.show();
+}
+
+String Button::getName(){
+  return name;
+}
+
+void Button::setMode(int _mode){
+  int newMode = write_mode(_mode);
+  if(newMode != mode) {
+    mode = newMode;
+    if(client != NULL && client->client->isConnected()){
+      String _name = name + "/mode";
+      client->Publish(_name.c_str(), String(mode));
+    }
+  }
+  switch(mode){
+    case 1:  setColor(255, 0, 0); break;
+    case 2:  setColor(0, 255, 0); break;
+    default: setColor(0, 0, 255); break;
+  }
+}
+
+int Button::read_mode(){
+  int ret = preferences.getInt(name.c_str(), 0);
+  if(ret < 0) ret = 0;
+  if(ret > 2) ret = 2;
+  Serial.println("eeprom readed: " + name + " = " + String(ret));
+  return ret;
+}
+
+int Button::write_mode(int _mode){
+  int ret = _mode;
+  if(ret < 0) ret = 0;
+  if(ret > 2) ret = 2;
+  if(ret != mode) {
+    preferences.putInt(name.c_str(), ret);
+    Serial.println("eeprom writed: " + name + " = " + String(ret));
+  }
+  return ret;
+}
+
+Button::Button(int _pin, int _num){
+  pin = _pin;
+  num = _num;
+  name = "B" + String(num);
+  pinMode(pin, INPUT);
+  mode = read_mode();
+  setMode(mode);
+}
+
+Button* btns[MAXNUMBUTTONS];
+
+stru_state read_eeprom(){
+  stru_state ret = curState;
+
+  ret.brightness = preferences.getInt("brgth", 127);
+  if(ret.brightness < 0) ret.brightness = 0;
+  if(ret.brightness > 255) ret.brightness = 255;
+  Serial.println("eeprom readed: brgth = " + String(ret.brightness));
+
+  ret.AutoSeconds = preferences.getInt("autosec", 2);
+  if(ret.AutoSeconds < 2) ret.AutoSeconds = 2;
+  if(ret.AutoSeconds > 10) ret.AutoSeconds = 10;
+  Serial.println("eeprom readed: autosec = " + String(ret.AutoSeconds));
+
+  return ret;
+}
+
+stru_state write_eeprom(stru_state newState){
+  stru_state ret = newState;
+
+  if(ret.brightness < 0) ret.brightness = 0;
+  if(ret.brightness > 255) ret.brightness = 255;
+  if(ret.brightness != curState.brightness) {
+    preferences.putInt("brgth", ret.brightness);
+    Serial.println("eeprom writed: brgth = " + String(ret.brightness));
+  }
+
+  if(ret.AutoSeconds < 2) ret.AutoSeconds = 2;
+  if(ret.AutoSeconds > 255) ret.AutoSeconds = 255;
+  if(ret.AutoSeconds != curState.AutoSeconds) {
+    preferences.putInt("autosec", ret.AutoSeconds);
+    Serial.println("eeprom writed: autosec = " + String(ret.AutoSeconds));
+  }
+
+  return ret;
+}
+
+//***
+void onMsgCommand( const String &message ){
+}
+
+void report(stru_state newState, int mode){
+  if (mode == 0 || ( mode == 1 && newState.brightness != curState.brightness ) ) {
+    client->Publish("settings/brightness", String(newState.brightness));
+  }
+
+  if (mode == 0 || ( mode == 1 && newState.AutoSeconds != curState.AutoSeconds ) ) {
+    client->Publish("settings/AutoSeconds", String(newState.AutoSeconds));
+  }
+
+  curState = newState;
+  client->flag_start = false;
+}
+
+void OnCheckState(){
+  stru_state newState = curState;
+
+  if(client->flag_start){ //первый запуск
+    report(newState, 0); //отправляем все
+  } else {
+    report(newState, 1); //отправляем только то, что изменилось
+  }
+}
+
+void Msg_brightness(const String &message){
+  stru_state newState = curState;
+  newState.brightness = message.toInt();
+  newState = write_eeprom(newState);
+  setBrightness(newState.brightness);
+  report(newState, 1);
+}
+
+void Msg_AutoSeconds(const String &message){
+  stru_state newState = curState;
+  newState.AutoSeconds = message.toInt();
+  newState = write_eeprom(newState);
+  report(newState, 1);
+}
+
+void Msg_btn0Mode(const String &message){
+  btns[0]->setMode(message.toInt());
+}
+void Msg_btn1Mode(const String &message){
+  btns[1]->setMode(message.toInt());
+}
+void Msg_btn2Mode(const String &message){
+  btns[2]->setMode(message.toInt());
+}
+void Msg_btn3Mode(const String &message){
+  btns[3]->setMode(message.toInt());
+}
+
+void onConnection()
+{
+  client->Subscribe("settings/brightness", Msg_brightness); 
+  client->Subscribe("settings/AutoSeconds", Msg_AutoSeconds); 
+  client->Subscribe(btns[0]->getName( ) + "/mode", Msg_btn0Mode); 
+  client->Subscribe(btns[1]->getName( ) + "/mode", Msg_btn1Mode); 
+  client->Subscribe(btns[2]->getName( ) + "/mode", Msg_btn2Mode); 
+  client->Subscribe(btns[3]->getName( ) + "/mode", Msg_btn3Mode); 
+}
+
+void OnLoad(){
+  client->flag_start = true;
+}
+
+void createButtons(){
+  for(int i=0; i<MAXNUMBUTTONS;i++){
+    btns[i] = new Button(pins[i], i);
+  }
+}
+
+void setBrightness(int brgth){
+  FastLED.setBrightness(brgth);
+  FastLED.show();
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Start...");
+
+  curState = read_eeprom();
+
+  createButtons();
+
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUMLEDS);
+  if (CURRENT_LIMIT > 0) FastLED.setMaxPowerInVoltsAndMilliamps(5, CURRENT_LIMIT);
+  FastLED.clear();
+  setBrightness(curState.brightness);
+
+  client = new mqtt_ini( 
+    "SWITCH01",
+     def_path);
+
+  client->begin(true);
+}
+
+void loop() {
+  client->loop();
+
+  for(int i=0; i<MAXNUMBUTTONS; i++){
+    btns[i]->loop();
+  }
+}
